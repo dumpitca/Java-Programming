c50663d98ac9f64168556b81da04cacfdb255d9c
dumpitca <69851613+dumpitca@users.noreply.github.com>
2020-08-24 14:10:20 +0800

    initial commit

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author kevin
+ */
+
+import java.util.Scanner;
+
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         String fname = sc.nextLine();
+         System.out.println(fname);
+         String lname = sc.nextLine();
+         System.out.println("Hello \n"+fname+" "+lname);
+         
+                 
+            
+            
+        
+         
+  }
+}
+
+        // TODO code application logic here
---
+++ Welcome/src/welcome/Welcome.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package welcome;
+
+/**
+ *
+ * @author kevin
+ */
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10; 
+        System.out.println("5 + 10 is equal to: ");
+        System.out.println(num1 + num2);// TODO code application logic here
+    }
+    
+}
