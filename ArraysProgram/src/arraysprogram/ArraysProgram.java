f85c304afa089ab3112208cfab662fbcdc9859a3
dumpitca <69851613+dumpitca@users.noreply.github.com>
2020-08-27 19:42:25 +0800

    added Arrays

---
+++ ArraysProgram/src/arraysprogram/ArraysProgram.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraysprogram;
+
+/**
+ *
+ * @author Bulitog
+ */
+public class ArraysProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] grades = {10, 11, 22, 33, 44, 55, 66}; //This is the syntax for making an arary in Java. There are other ways but this is recommended.
+        //Inside the brackets "10" is the numbers of arrays. Arrays always starts with 0.
+        //Hte numbers of arrays would always be one less of the declared value. 
+        //Ex. 10 ang inassign mo yung visual representation ng array non is 0-9 lang kasi sa "0" nag sastart ang arrays. 
+        //line 18 is the shortest way of making an array. 
+        
+        System.out.println(grades[1]);
+
+
+
+
+
+
+
+
+
+// TODO code application logic here
+    }
+    
+}
