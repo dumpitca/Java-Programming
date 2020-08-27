6e3258729c557acd37386874bab9eafc3a7eeaf6
dumpitca <69851613+dumpitca@users.noreply.github.com>
2020-08-27 23:18:02 +0800

    added ArraysProgrampt2

---
+++ ArraysProgrampt2/src/arraysprogrampt2/ArraysProgrampt2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraysprogrampt2;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Bulitog
+ */
+public class ArraysProgrampt2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] grades = {10, 11, 22, 33, 44, 55, 66};
+        System.out.println(Arrays.toString(grades)); 
+//if you print System.out.println(grades) you will get an input of [I@15db9742 and that's not what we want. 
+//In printing an array, use System.out.println(Arrays.toString(variable_name))
+
+
+
+// TODO code application logic here
+    }
+    
+}
