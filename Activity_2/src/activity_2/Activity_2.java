39bd844328539a8085b4e16a4a270586673aa09c
dumpitca <69851613+dumpitca@users.noreply.github.com>
2020-09-04 23:43:20 +0800

    added acitivity 2

---
+++ Activity_2/src/activity_2/Activity_2.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity_2;
+
+/**
+ *
+ * @author Bulitog
+ */
+public class Activity_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //int x =1;
+       // int y = -2;
+       // int z = -6;
+        //int a = 0;
+        
+       // z%= 2;
+       // a = ++z;
+        //x +=y;
+        //System.out.println("x: " + x);
+       // System.out.println("y: " + y);
+        //System.out.println("z: " + z);
+        //System.out.println("a: " + a);
+        
+        
+       // int a = 1;
+       // int b = -3;
+       // int c = 10;
+       // int d = 2;
+       // int e;
+       // int f;
+        
+       // e = ++a;
+       // b *= c;
+       // d += 2 +c;
+       // f = c++;
+        
+      //  System.out.println("a: " + a);
+       // System.out.println("b: " + b);
+       // System.out.println("c: " + c);
+       // System.out.println("d: " + d);
+       // System.out.println("e: " + e);
+       // System.out.println("f: " + f);
+       
+       
+       //===================================
+       
+       double pi = 3.14;
+       int a = 2;
+       int c = 1;
+       double r = Math.pow(c, 0.66);
+       int n = 3;
+       int b = (int) Math.pow(n, 3); //for the exponent 3
+       double x = 0.5;
+       double s = Math.pow(5, x);
+       int f = 4;
+       double d = Math.pow(a, x);
+       
+       
+       double first_term = (pi * a * (r)) / a * (n *(b) + 2);
+       double second_term_partA = (3 + ((s) / f) + 2);
+       double second_term = second_term_partA * ((d) / n);   
+       
+        System.out.println(first_term - second_term);
+            
+               
+       
+       
+      
+    }
+    
+    
+}
