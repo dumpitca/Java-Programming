af6647ce9190237d12ed0247c6c0c49ad3b77f30
dumpitca <69851613+dumpitca@users.noreply.github.com>
2020-09-22 11:50:16 +0800

    added activity 1

---
+++ Activity1/Activity1pt2/src/activity1pt2/Activity1pt2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity1pt2;
+
+/**
+ *
+ * @author Bulitog
+ */
+public class Activity1pt2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //int x = 12;
+       // int convert = (2 * (x * x)) + (4 * x) - 6; //number 1
+         //v == 3.14 * (r * r) //number 2
+           //((3.14 * (r * r) / (f * f * f * f)) + 2) - ((s * s) / 4)) + 2 (2 /3) //number 3       
+           //System.out.println("converted: " + convert);
+           
+         int x = 3;
+         ++x;
+         System.out.println(x);
+
+
+
+
+
+
+
+// TODO code application logic here
+    }
+    
+}
---
+++ Activity1/src/activity1/Activity1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity1;
+
+import java.io.PrintStream;
+
+/**
+ *
+ * @author Bulitog
+ */
+public class Activity1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {      
+        boolean guessMeBaby = ( ! ( false && true ) || ( ! false ||! true ) ) ;
+        boolean guessAgainBaby = ! ( ! true && ! ( 3 !=2 || (4 == 4 ) ) && ! ( 5 < 2 ) );
+        System.out.println(guessMeBaby);
+        System.out.println(guessAgainBaby);
+        //int a = 3;
+        //int b = -2;
+        //int c = 10;
+        //int d = -32;
+        // a = d;
+        // d = c;
+        //c = a + 2;
+        //b = b * c + 2;
+        //d = a % b;
+        //System.out.println("a:" + a);
+        //System.out.println("b:" + b);
+        //System.out.println("c:" + c);
+        // System.out.println("d:" + d);
+// TODO code application logic here
+    }
+    
+}
