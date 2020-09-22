4274a9c890412874c3ca068e585c0be3bee85c77
dumpitca <69851613+dumpitca@users.noreply.github.com>
2020-09-22 11:55:46 +0800

    added if-else if-else

---
+++ If-Else If-Else/src/pkgif/pkgelse/pkgif/pkgelse/IfElseIfElse.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkgelse.pkgif.pkgelse;
+
+/**
+ *
+ * @author Bulitog
+ */
+public class IfElseIfElse {
+
+    public static void displayFalse() {
+        System.out.println("The statements are false");
+    }
+    
+   
+   
+    public static void main(String[] args) {
+        int number1 = 28;
+        int number2 = 28;
+        
+        if (number1 == number2) {
+            System.out.println("The IF statement will be printed"); //If the first IF statement is true this lne will be printed 
+       
+        }else {
+               displayFalse(); 
+        
+        } 
+        
+        
+        if (number1 >= 27) {
+            System.out.println("Number1 is greater than or equal to 27");
+        
+        }
+    }
+    
+}
---
+++ If-Else If-Else/src/pkgif/pkgelse/pkgif/pkgelse/IfElse_pt2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkgelse.pkgif.pkgelse;
+
+/**
+ *
+ * @author Bulitog
+ */
+public class IfElse_pt2 {
+    
+    public static void hello() {
+        System.out.println("hello()");
+    
+    }
+    
+    public static void main (String [] args) {
+        System.out.println("start of main");
+        int number1 = 4;
+        
+        if (number1 == 2) {
+            System.out.println("is equal to 2");
+    
+    }   else if (number1 >= 3) {
+            System.out.println("number1 is equal to 3");
+            
+    }   else if (number1 <= 1) {
+          System.out.println("number1 is less than or equal to 1");
+    
+    }   else {
+            System.out.println("all statements are false");
+        
+    }
+            
+    
+    }
+
+
+}
+
+
+
